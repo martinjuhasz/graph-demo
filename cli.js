6e9630953eaf36667208c529e1331559edd09dba
Markus Tacker <m@cto.hiv>
2016-05-09 12:16:21 +0200

    chore(CLI): implement networhk generator

--- cli.js
+++ cli.js
@@ -1,36 +1,74 @@
 'use strict'
 
-let yargs = require('yargs')
-let Promise = require('bluebird')
-let _forEach = require('lodash/forEach')
+const program = require('commander')
+const Promise = require('bluebird')
+const _map = require('lodash/map')
+const _filter = require('lodash/filter')
+const _forIn = require('lodash/forIn')
 
-function run () {
-  var argv = yargs
-    .usage('Usage: $0 <command> [options]')
-    .demand(1)
-    .command('networhk-generate <people> <contributions>', 'generate a networhk')
-    .help()
-    .argv
-
-  switch (argv._[0]) {
-    case 'networhk-generate':
+program
+  .command('networhk-generate <people> <contributions>')
+  .description('generate a netwoRHk')
+  .action(
+    (people, contributions) => {
       return Promise
         .try(() => {
-          let generator = require('./src/networhk-generator')(+argv.people, +argv.contributions)
-          _forEach(generator.people, (person) => {
-            console.log(person.email, person.name)
+          let netwoRHk = require('./src/networhk-generator')(+people, +contributions)
+          let data = {
+            users: [],
+            contributions: []
+          }
+          let contributionCommitments = {}
+          _forIn(netwoRHk.people, (user) => {
+            let [firstname, lastname] = user.name.split(' ')
+            data.users.push({
+              email: user.email.toLowerCase(),
+              firstname,
+              lastname
+            })
+            _map(user.commitments, (commitment) => {
+              if (!contributionCommitments[commitment.contribution]) {
+                contributionCommitments[commitment.contribution] = []
+              }
+              contributionCommitments[commitment.contribution].push(user.email.toLowerCase())
+            })
           })
+          _forIn(netwoRHk.contributions, (contribution) => {
+            let ownerEmail = netwoRHk.people[contribution.creator].email.toLowerCase()
+            let c = {
+              title: contribution.title,
+              description: contribution.title,
+              open: contribution.open,
+              priority: contribution.priority,
+              commitments: [
+                {
+                  user: ownerEmail,
+                  status: contribution.status,
+                  owner: true
+                }
+              ]
+            }
+            let userCommitments = _filter(contributionCommitments[contribution.id], (email) => {
+              return email !== ownerEmail
+            })
+            _map(userCommitments, (email) => {
+              c.commitments.push({
+                user: email,
+                status: Math.random() > 0.33 ? 'good' : 'bad'
+              })
+            })
+            data.contributions.push(c)
+          })
+          console.log(JSON.stringify(data, null, '  '))
         })
-    default:
-      throw new Error('Unknown command: ' + argv._[0])
-  }
-}
+        .then(() => {
+          process.exit(0)
+        })
+        .catch((err) => {
+          console.error(err)
+          process.exit(1)
+        })
+    }
+  )
 
-run()
-  .then(function () {
-    process.exit(0)
-  })
-  .catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+program.parse(process.argv)
